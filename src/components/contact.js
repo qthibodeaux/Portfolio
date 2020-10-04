e614c03d5580352784d03b049b04279d35421b21
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2020-10-03 22:06:52 -0500

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,7 +12,7 @@ function Contact () {
     return (
         <div className="d-flex flex-column" id="contactanchor" style={MyStyle} >
             <h5 className="text-center pt-6">Thank you for visiting my site.</h5>
-            <h6 className="text-center">Quintus Thibodeaux asdf</h6>
+            <h6 className="text-center">Quintus Thibodeaux</h6>
             <h6>Email: qthibgit@gmail.com</h6>
             <h6>Phone: 979-221-6149</h6>
             <a href="https://www.linkedin.com/in/quintus-thibodeaux-4372751b0/" target="_blank" rel="noopener noreferrer">Linkedin</a>
